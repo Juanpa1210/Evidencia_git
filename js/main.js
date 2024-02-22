2ff209a6e344c45d344283c5320cd4a87efef703
Juan Chavarro <imjpaul2001@gmail.com>
2024-02-22 15:30:01 -0500

    actualizacion de contenido en documentos

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//titulo y parrafos
+
+document.getElementById("nombre").innerText='Juan pablo'
+document.getElementById("apellido").innerText='Chavarro Pachon'
+document.getElementById("telefono").innerText='3013457642'
+document.getElementById("correo").innerText='imjpaul2001gmail.com'
+document.getElementById("edad").innerText='21'
+document.getElementById("hobby").innerText='mis hobby`s hacer ejercicio, jugar en el pc, practicar con la guitarra, manejar moto'
